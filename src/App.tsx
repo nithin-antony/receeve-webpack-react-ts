4b1ec7ac8922c39946e732cfd3637d94a7255d96
Nithin Antony <nithinparathara007@gmail.com>
2022-07-16 22:50:53 +0530

    adding storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Table from './components/Table/Table'
+
 import './index.scss'
 
 export const App = () => {
-  const [count, setCount] = useState(0)
   return (
     <>
-      <h1>React Typescript Webpack</h1>
-      <button name="Increment" onClick={() => setCount(count + 1)} />
-      <h2>Count value: {count}</h2>
+      <Table></Table>
     </>
   )
 }
